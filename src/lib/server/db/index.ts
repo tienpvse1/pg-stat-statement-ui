b52eaaca60cbbf012770eba2a4397b0670e35d5a
tienpvse1 <good66612@gmail.com>
2025-04-22 12:08:54 +0700

    resolved: replace drizzle with kysely

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,21 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import * as schema from './schema';
+import { Pool } from 'pg';
+import { Kysely, PostgresDialect } from 'kysely';
 import { env } from '$env/dynamic/private';
+import type { DB } from './db';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+const dialect = new PostgresDialect({
+	pool: new Pool({
+		user: env.POSGTGRES_USER,
+		password: env.POSTGRES_PASSWORD,
+		port: +env.POSTGRES_PORT,
+		database: env.POSTGRES_DB
+	})
+});
 
-const client = postgres(env.DATABASE_URL);
-
-export const db = drizzle(client, { schema });
+// Database interface is passed to Kysely's constructor, and from now on, Kysely
+// knows your database structure.
+// Dialect is passed to Kysely's constructor, and from now on, Kysely knows how
+// to communicate with your database.
+export const db = new Kysely<DB>({
+	dialect
+});
